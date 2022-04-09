7b1a288f025d4656b436af31ce6c017a2a5b921b
Eddy-scion <justiceogbevon@gmail.com>
2022-04-09 22:55:43 +0200

    first commit

---
+++ chart.js
@@ -0,0 +1,21 @@
+document.querySelector('.empty1').addEventListener( 'click', function (){
+    document.querySelector('.highlight').style.left = '200px';
+ })
+
+ document.querySelector('.empty2').addEventListener( 'click', function (){
+    document.querySelector('.highlight').style.left = '357px';
+ })
+ 
+ document.querySelector('.empty3').addEventListener( 'click', function (){
+    document.querySelector('.highlight').style.left = '535px';
+ })
+
+ document.querySelector('.empty4').addEventListener( 'click', function (){
+    document.querySelector('.highlight').style.left = '695px';
+ })
+
+ document.querySelector('.empty5').addEventListener( 'click', function (){
+    document.querySelector('.highlight').style.left = '850px';
+ })
+
+
